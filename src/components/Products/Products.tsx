149bffcf59e6f5ba073c0a52fb5cdc537a90a4e7
martinmilev <martin@martinmilevs-MacBook-Air.local>
2024-06-27 16:04:41 +0300

    added images for different types of coffee

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,22 +1,29 @@
 const Products = ({ products, handleClick, disabled }) => {
   return (
     <div className="row">
-        <div className="grid">
-          {products.map((product) => (
-            <button
-              key={product.id}
-              onClick={() => handleClick(product)}
-              className={`grid-button`}
-              disabled={disabled}
-            >
-              <p>
+      <div className="grid">
+        {products.map((product) => (
+          <button
+            key={product.id}
+            onClick={() => handleClick(product)}
+            className={`grid-button`}
+            disabled={disabled}
+          >
+            <div className="product-content">
+              <img
+                src={`src/assets/images/${product.image}`}
+                alt={""}
+                className="product-image"
+              />
+              <p className="product-info">
                 {product.name} <br />
                 {product.price}€
               </p>
-            </button>
-          ))}
-        </div>
+            </div>
+          </button>
+        ))}
       </div>
+    </div>
   );
 };
 
