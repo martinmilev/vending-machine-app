40fd3c5c71bf44dcc4acc2312ac33fb282c05323
martinmilev <martin@martinmilevs-MacBook-Air.local>
2024-06-28 10:35:04 +0300

    fixed pictures not loading in prod

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   if (loading) {
     return (
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Product } from "../../ts/types/product";
 
 interface ProductsProps {
@@ -10,31 +11,44 @@ const Products: React.FC<ProductsProps> = ({
   products,
   handleClick,
   disabled,
-}) => (
-  <div className="row">
-    <div className="grid">
-      {products.map((product) => (
-        <button
-          key={product.id}
-          onClick={() => handleClick(product)}
-          className={`grid-button`}
-          disabled={disabled}
-        >
-          <div className="product-content">
-            <img
-              src={`src/assets/images/${product.image}`}
-              alt={""}
-              className="product-image"
-            />
-            <p className="product-info">
-              {product.name} <br />
-              {product.price}€
-            </p>
-          </div>
-        </button>
-      ))}
+}) => {
+  //temporary fix
+  const getUrl = (): string => {
+    switch (import.meta.env.MODE) {
+      case "production":
+        return "https://raw.githubusercontent.com/martinmilev/vending-machine-app/master/";
+
+      default:
+        return "/";
+    }
+  };
+
+  return (
+    <div className="row">
+      <div className="grid">
+        {products.map((product) => (
+          <button
+            key={product.id}
+            onClick={() => handleClick(product)}
+            className={`grid-button`}
+            disabled={disabled}
+          >
+            <div className="product-content">
+              <img
+                src={`${getUrl()}src/assets/images/${product.image}`}
+                alt={""}
+                className="product-image"
+              />
+              <p className="product-info">
+                {product.name} <br />
+                {product.price}€
+              </p>
+            </div>
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Products;
